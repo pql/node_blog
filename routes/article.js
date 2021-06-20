bc7bdae0fde6959928c5fe4889f2a52214ece186
pql <1101192991@qq.com>
2021-06-20 17:13:44 +0800

    实现更新文章功能

--- routes/article.js
+++ routes/article.js
@@ -5,7 +5,8 @@ let { Article } = require('../model');
 
 router.get('/add',checkLogin, function(req, res) {
     res.render('article/add', {
-        title: '发表文章'
+        title: '发表文章',
+        article: {}
     });
 });
 
@@ -47,6 +48,33 @@ router.get('/delete/:_id', function(req, res){
         } else {
             req.flash('success', '删除文章成功');
             res.redirect('/');
+        }
+    });
+});
+
+// 当点击编辑按钮的时候，会请求此路由
+router.get('/update/:_id', function(req, res) {
+    let _id = req.params._id;
+    Article.findById(_id, function(err, article) {
+        // 增加和更新可以复用一个模板
+        res.render('article/add', {
+            title: '更新文章',
+            article: article
+        })
+    });
+});
+
+router.post('/update/:_id', function(req, res){
+    let _id = req.params._id;
+    Article.update({
+        _id
+    }, req.body, function(err, result) {
+        if(err) {
+            req.flash('error', err);
+            res.redirect('back');
+        } else {
+            req.flash('success', "文章更新成功");
+            res.redirect('/article/detail/'+_id);
         }
     });
 });
